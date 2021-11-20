4a3cd8751db8757f392fef6f160e8eb4c8eb1dd2
Jorge Castro <jorgs705@gmail.com>
2021-11-20 07:49:00 -0600

    update 1.6

--- src/pages/GeneralInformation/Information.jsx
+++ src/pages/GeneralInformation/Information.jsx
@@ -7,7 +7,7 @@ const Information = ({information}) => {
     const { bio, followers, following, name, login, public_repos, avatar_url } = information;
 
     return (
-        <div className='cart'>
+        <div className='cart-info'>
             <img src={avatar_url} alt='img' 
                 className='size-img'
             />
@@ -17,13 +17,13 @@ const Information = ({information}) => {
                 {login} <br />
                 <section className='sec-links'>
                     <Link to={`/followers/${login}`}>
-                        <div className='link-d flex'>followers {followers} </div>
+                        <div className='link-d flex-l'>followers {followers} </div>
                     </Link>
-                    <div className='link-d flex'>
+                    <div className='link-d flex-l'>
                         follogin {following} <br />
                     </div>
                     <Link to={`/repositories/:${login}`}>
-                        <div className='link-d flex'>repos {public_repos} </div>
+                        <div className='link-d flex-l'>repos {public_repos} </div>
                     </Link>     
                 </section>       
             </section>
