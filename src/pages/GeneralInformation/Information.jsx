ca80ae92b6f65565d6249510e8a6f5c5bd3c92a4
Jorge Castro <jorgs705@gmail.com>
2021-11-19 23:35:30 -0600

    update 1.4

--- src/pages/GeneralInformation/Information.jsx
+++ src/pages/GeneralInformation/Information.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Information.styles.css';
 
 const Information = ({information}) => {
 
     const { bio, followers, following, name, login, public_repos, avatar_url } = information;
 
     return (
-        <div>
-            <img src={avatar_url} alt='img' />
-            {bio} <br />
-            {name} <br />
-            {login} <br />
-            <Link to={`/followers/${login}`}>
-                followers {followers} <br />
-            </Link>
-            follogin {following} <br />
-            repos {public_repos}
+        <div className='cart'>
+            <img src={avatar_url} alt='img' 
+                className='size-img'
+            />
+            <section className='information'>
+                {bio} <br />
+                {name} <br />
+                {login} <br />
+                <Link to={`/followers/${login}`}>
+                    followers {followers} <br />
+                </Link>
+                follogin {following} <br />
+                repos {public_repos}
+            </section>
         </div>
     )
 };
